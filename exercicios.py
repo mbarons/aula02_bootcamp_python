86f54db7c400ec7bb41583c11c21a548e0f93549
mbarons <57764271+mbarons@users.noreply.github.com>
2025-06-25 23:46:51 -0300

    terceiro exercicio resolvido

--- exercicios.py
+++ exercicios.py
@@ -15,6 +15,15 @@
 
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+# x = int(input("Digite um número: "))
+# y = int(input("Digite outro número: "))
+
+# resultado = x * y
+
+# print(resultado)
+
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
